00f6f2985a2901b89fca3f441ee4b1705aebe1ec
jasonliuhe <walkerliuhe@gmail.com>
2021-05-17 13:13:59 -0400

    Adding backend Code with API Routes (Fullstack React)

---
+++ 8/nextjs-course-code/pages/feedback/index.js
@@ -0,0 +1,35 @@
+import { buildFeedbackPath, extractFeedback } from '../api/feedback';
+import {Fragment, useState} from 'react';
+
+function FeedbackPage(props) {
+    const [feedbackData, setFeedbackData] = useState();
+
+    function loadFeedbackHandler(id) {
+        fetch(`/api/${id}`).then(response => response.json()).then(data => {
+            setFeedbackData(data.feedback);
+        })
+    }
+
+    return (
+        <Fragment>
+            {feedbackData && <p>{feedbackData.email}</p>}
+            <ul>
+            {props.feedbackItems.map((item) => (
+                <li key={item.id}>{item.id}<button onClick={loadFeedbackHandler.bind(null, item.id)}>Show Details</button></li>
+            ))}
+            </ul>
+        </Fragment>
+    );
+}
+
+export async function getStaticProps() {
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+    return {
+        props: {
+            feedbackItems: data,
+        }
+    };
+}
+
+export default FeedbackPage;
